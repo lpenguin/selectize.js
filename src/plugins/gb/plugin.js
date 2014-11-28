c45cd8a8ac8782dca41422bdef39447cfd0e18e5
n.pryanichnikov <n.pryanichnikov@vectordigital.ru>
2014-11-28 18:13:56 +0300

    gb plugin and example page

---
+++ src/plugins/gb/plugin.js
@@ -0,0 +1,32 @@
+Selectize.define('gb', function(options) {
+  var self = this;
+
+  // options = $.extend({
+  //   title         : 'Untitled',
+  //   headerClass   : 'selectize-dropdown-header',
+  //   titleRowClass : 'selectize-dropdown-header-title',
+  //   labelClass    : 'selectize-dropdown-header-label',
+  //   closeClass    : 'selectize-dropdown-header-close',
+  //
+  //   html: function(data) {
+  //     return (
+  //       '<div class="' + data.headerClass + '">' +
+  //       '<div class="' + data.titleRowClass + '">' +
+  //       '<span class="' + data.labelClass + '">' + data.title + '</span>' +
+  //       '<a href="javascript:void(0)" class="' + data.closeClass + '">&times;</a>' +
+  //       '</div>' +
+  //       '</div>'
+  //     );
+  //   }
+  // }, options);
+  //
+  // self.setup = (function() {
+  //   var original = self.setup;
+  //   return function() {
+  //     original.apply(self, arguments);
+  //     self.$dropdown_header = $(options.html(options));
+  //     self.$dropdown.prepend(self.$dropdown_header);
+  //   };
+  // })();
+
+});
