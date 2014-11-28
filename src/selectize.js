e1a1a5de043366dbbdf5972ff230727c771666cc
n.pryanichnikov <n.pryanichnikov@vectordigital.ru>
2014-11-28 17:43:55 +0300

    Support for optgroup from search result

--- src/selectize.js
+++ src/selectize.js
@@ -1008,7 +1008,7 @@ $.extend(Selectize.prototype, {
 		for (i = 0; i < n; i++) {
 			option      = self.options[results.items[i].id];
 			option_html = self.render('option', option);
-			optgroup    = option[self.settings.optgroupField] || '';
+			optgroup    = option[self.settings.optgroupField] || results.items[i].optgroup || '';
 			optgroups   = $.isArray(optgroup) ? optgroup : [optgroup];
 
 			for (j = 0, k = optgroups && optgroups.length; j < k; j++) {
